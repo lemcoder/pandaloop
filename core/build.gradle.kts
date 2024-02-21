865d1d8ffd65edfac45ea3fba6ac26e0f766e3da
lenovo <lenovo@home.pl>
2024-02-21 22:17:10 +0100

    Initial commit

---
+++ core/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.library)
+}
+
+kotlin {
+    jvmToolchain(17)
+
+    androidTarget()
+
+    sourceSets {
+        commonMain.configure { }
+        commonTest.configure {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+
+        val androidInstrumentedTest by getting {
+            dependencies {
+                implementation(libs.androidX.testRunner)
+            }
+        }
+    }
+}
+
+android {
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+        multiDexEnabled = true
+        namespace = "pl.lemanski.pandaloop"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        ndk {
+            abiFilters += listOf("x86", "x86_64", "armeabi-v7a", "arm64-v8a")
+        }
+    }
+
+    sourceSets {
+        getByName("main") {
+            manifest.srcFile("src/androidMain/AndroidManifest.xml")
+        }
+    }
+
+    externalNativeBuild {
+        cmake {
+            path = file("src/androidMain/CMakeLists.txt")
+        }
+    }
+}
---
+++ core/src/androidMain/kotlin/Sha256.kt
@@ -0,0 +1,14 @@
+package pl.lemanski.pandaloop
+
+actual object Sha256Factory {
+    actual fun createEncoder(): Sha256Encoder = AndroidSha256Encoder
+}
+
+object AndroidSha256Encoder : Sha256Encoder {
+
+    init {
+        System.loadLibrary("sha256")
+    }
+
+    external override fun encode(src: ByteArray): ByteArray
+}
