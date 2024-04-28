7875abbd6ebe280d5ab059437703af19a0747598
Mikołaj Lemański <mikolajlem@gmail.com>
2024-04-28 22:09:19 +0200

    Fixed time signature

--- core/src/commonMain/kotlin/pl/lemanski/pandaloop/TimeSignature.kt
+++ core/src/commonMain/kotlin/pl/lemanski/pandaloop/TimeSignature.kt
@@ -1,5 +1,6 @@
 package pl.lemanski.pandaloop
 
+import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
 
 sealed interface TimeSignature {
@@ -8,14 +9,14 @@ sealed interface TimeSignature {
 
 data object Common : TimeSignature {
     override fun getTimeWithTempo(tempo: Int): Int {
-        val beat = 1.seconds.inWholeMilliseconds.toInt() / tempo
+        val beat = 1.minutes.inWholeMilliseconds.toInt() / tempo
         return 4 * beat
     }
 }
 
 data object ThreeFours : TimeSignature {
     override fun getTimeWithTempo(tempo: Int): Int {
-        val beat = 1.seconds.inWholeMilliseconds.toInt() / tempo
+        val beat = 1.minutes.inWholeMilliseconds.toInt() / tempo
         return 3 * beat
     }
 }
