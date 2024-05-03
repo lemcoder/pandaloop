57d973c762659bfd7b0dbf786a3bc454c73dd1e6
Mikołaj Lemański <mikolajlem@gmail.com>
2024-05-04 00:57:54 +0200

    Added range check in millisToFrames

--- core/src/commonMain/kotlin/pl/lemanski/pandaloop/utils/Utils.kt
+++ core/src/commonMain/kotlin/pl/lemanski/pandaloop/utils/Utils.kt
@@ -3,4 +3,4 @@ package pl.lemanski.pandaloop.utils
 import pl.lemanski.pandaloop.engine.getChannelCount
 import pl.lemanski.pandaloop.engine.getSampleRate
 
-internal fun millisToFrames(millis: Int): Int = getSampleRate() * (millis / 1_000) * getChannelCount()
+internal fun millisToFrames(millis: Int): Int = getSampleRate() * (millis.coerceIn(0, Int.MAX_VALUE) / 1_000) * getChannelCount()
